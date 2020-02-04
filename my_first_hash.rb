b90d0b113215a569eb3331c7be81cdb21e0d9357
Lakeisha McCree <lmccree215@icloud.com>
2020-02-04 03:40:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,5 +16,10 @@ def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
   shipping_manifest["muskets"] = 2
+  
+  shipping_manifest["gun_powder"] = 4
+  
   shipping_manifest
+  
+  
 end
