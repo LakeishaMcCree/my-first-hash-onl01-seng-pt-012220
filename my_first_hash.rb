8625ea4a782aeb07c70fbfaa664c0138dd83d2f7
Lakeisha McCree <lmccree215@icloud.com>
2020-02-04 03:30:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,10 +13,5 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  shipping_manifest = {["pearl necklace"] = 1
-                       ["gun powder"] = 4
-                      ["muskets"] = 2
-  }
+  
 end
